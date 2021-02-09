bc5cbf5b44ce8859f2f151ac210b905ffef26266
Antoine <aaltorffer@orange.fr>
2021-02-09 12:06:31 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AkaViewer
+
+3D GLTF viewer based on [Aka engine](https://github.com/antaalt/Aka).
---
+++ src/ModelViewer.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <Aka/Aka.h>
+
+#include "Model/ModelLoader.h"
+
+namespace viewer {
+
+class Viewer : public aka::Application
+{
+	void loadShader();
+public:
+	void initialize() override;
+	void destroy() override;
+	void update(Time::Unit deltaTime) override;
+	void render() override;
+private:
+	aka::ShaderMaterial::Ptr m_material;
+	aka::Shader::Ptr m_shader;
+	Model::Ptr m_model;
+	ArcballCamera m_camera;
+};
+
+};
